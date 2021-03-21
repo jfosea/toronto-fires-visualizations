d3f3086677d75b5e663ab68a636ac1d0c9126f37
josea <janaosea.25@gmail.com>
2021-03-20 23:49:34 -0600

    created 3 vis

--- visualization 1/vis1.js
+++ visualization 1/vis1.js
@@ -2,10 +2,10 @@ var w = 900,
     h = 600,
     x = -14100,
     y = 7300,
-    scale=66700;
+    scale = 66700;
 
 var projection = d3.geo.albers()
-    .translate([x,y])
+    .translate([x, y])
     .scale([scale]);
 
 var path = d3.geo.path()
@@ -13,36 +13,23 @@ var path = d3.geo.path()
 
 var svg = d3.select("#map")
     .append("svg")
-    .attr("width",w)
-    .attr("height",h);
+    .attr("width", w)
+    .attr("height", h);
 
 var paths = svg.append("g");
 
-// d3.json("toronto.json",function(error, data){
-//
-//     paths.selectAll("area")
-//         .data(data.features)
-//         .enter()
-//         .append("path")
-//         .attr("d",path)
-//         .attr("fill", "#b5b4b4")
-//         .attr("stroke-width", "1")
-//         .attr("stroke", "#ffffff")
-// });
-
-
-d3.json("toronto.json",function(data){
+d3.json("toronto.json", function (data) {
 
     paths.selectAll("area")
         .data(data.features)
         .enter()
         .append("path")
-        .attr("d",path)
-        .attr("fill", "#b5b4b4")
+        .attr("d", path)
+        .attr("fill", "#dddddd")
         .attr("stroke-width", "1")
         .attr("stroke", "#ffffff")
 
-    d3.csv("final.csv", function(data) {
+    d3.csv("final.csv", function (data) {
         console.log(data);
 
         paths.selectAll("fire")
@@ -50,32 +37,34 @@ d3.json("toronto.json",function(data){
             .enter()
             .append("circle")
             .attr("class", "fire")
-            .attr("r", function(d) {
-                var cost = d.Estimated_Dollar_Loss / 15000
+            .attr("r", function (d) {
+                var cost = Math.log(d.Estimated_Dollar_Loss)
                 return cost
             })
-            .attr("cx", function(d) {
+            .attr("cx", function (d) {
                 var coords = projection([d.Longitude, d.Latitude])
                 return coords[0]
             })
-            .attr("cy", function(d) {
+            .attr("cy", function (d) {
                 var coords = projection([d.Longitude, d.Latitude])
                 return coords[1]
             })
-            .attr("fill", function(d) {
+            .attr("fill", function (d) {
                 var cause = d.Ignition_Source
                 if (cause == "appliance") {
-                    return "#911212"
+                    return "#58d454"
                 } else if (cause == "candle") {
-                    return "#85c149"
-                } else {
-                    return "#0d76e0"
+                    return "#30bac5"
+                } else if (cause == "electronic wiring") {
+                    return "#ffd214"
+                } else if (cause == "heater") {
+                    return "#ff8029"
+                } else if (cause == "lightbulb") {
+                    return "#df0000"
+                } else if (cause == "smoker's article") {
+                    return "#4f40d6"
                 }
             })
             .attr("opacity", "0.7")
     });
 });
-
-
-
-
